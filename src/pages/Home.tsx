a034ccb58c90d5815bb5c7f04187572178a268bc
loxbezsaita <bot99980@gmail.com>
2026-05-27 18:21:16 +0700

    q

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,7 +32,63 @@ export const Home = () => {
             <Link to="/new">New Arrival</Link>
             <Link to="/about">About</Link>
           </nav>
-          <div className="header__search"></div>
+          <div className="header__search">
+            <Link to="/search">
+              <svg
+                width="14"
+                height="14"
+                viewBox="0 0 14 14"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M12.75 12.75L9.28553 9.28553M9.28553 9.28553C10.1904 8.38071 10.75 7.13071 10.75 5.75C10.75 2.98858 8.51142 0.75 5.75 0.75C2.98858 0.75 0.75 2.98858 0.75 5.75C0.75 8.51142 2.98858 10.75 5.75 10.75C7.13071 10.75 8.38071 10.1904 9.28553 9.28553Z"
+                  stroke="#B6B6B6"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+              Search
+            </Link>
+            <svg
+              width="17"
+              height="21"
+              viewBox="0 0 17 21"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M11.9989 4.5C11.9989 6.57107 10.3199 8.25 8.24886 8.25C6.1778 8.25 4.49886 6.57107 4.49886 4.5C4.49886 2.42893 6.1778 0.75 8.24886 0.75C10.3199 0.75 11.9989 2.42893 11.9989 4.5Z"
+                stroke="white"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+              <path
+                d="M0.75 18.6182C0.820305 14.5369 4.15074 11.25 8.24886 11.25C12.3471 11.25 15.6776 14.5371 15.7477 18.6185C13.4649 19.666 10.9253 20.25 8.24918 20.25C5.57286 20.25 3.03296 19.6659 0.75 18.6182Z"
+                stroke="white"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+            <svg
+              width="21"
+              height="20"
+              viewBox="0 0 21 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M0.75 0.75H2.13568C2.64537 0.75 3.09138 1.09265 3.2227 1.58513L3.6059 3.02209M6 12C4.34315 12 3 13.3431 3 15H18.75M6 12H17.2183C18.3394 9.69936 19.3177 7.31635 20.1417 4.8625C15.38 3.64646 10.3905 3 5.25 3C4.70021 3 4.15214 3.0074 3.6059 3.02209M6 12L3.6059 3.02209M4.5 18C4.5 18.4142 4.16421 18.75 3.75 18.75C3.33579 18.75 3 18.4142 3 18C3 17.5858 3.33579 17.25 3.75 17.25C4.16421 17.25 4.5 17.5858 4.5 18ZM17.25 18C17.25 18.4142 16.9142 18.75 16.5 18.75C16.0858 18.75 15.75 18.4142 15.75 18C15.75 17.5858 16.0858 17.25 16.5 17.25C16.9142 17.25 17.25 17.5858 17.25 18Z"
+                stroke="white"
+                stroke-width="1.5"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              />
+            </svg>
+          </div>
         </header>
       </div>
     </main>
