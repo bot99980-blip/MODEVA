020155b105d3e3a325a1ffa8ec32b59faa24ab74
loxbezsaita <bot99980@gmail.com>
2026-05-27 18:08:56 +0700

    q

---
+++ src/components/Discount.tsx
@@ -0,0 +1,9 @@
+import "../styles/discount.scss"
+
+export const Discount = () => {
+    return (
+        <div className="discount">
+            <p>Discount 20% For New Member, <span> ONLY FOR TODAY</span>!!</p>
+        </div>
+    )
+}
